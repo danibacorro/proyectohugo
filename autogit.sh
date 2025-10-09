a4861085a37fab9d064ea0692b1cc29edd813947
Dani <danibacorro@gmail.com>
2025-10-09 14:14:10 +0200

    \nActualización del proyecto: jue 09 oct 2025 14:14:10 CEST\n

--- autogit.sh
+++ autogit.sh
@@ -7,35 +7,35 @@
 set -e
 
 # Subir proyecto
-echo "\nSincronizando proyecto en GitHub...\n"
+echo -e "\nSincronizando proyecto en GitHub...\n"
 cd ~/proyectohugo
 git add .
 
 # Solo hace commit si hay cambios, necesario con set -e
 if git diff-index --quiet HEAD --; then
-  echo "\nSin cambios que subir.\n"
+  echo -e "\nSin cambios que subir.\n"
 else
   git commit -m "\nActualización del proyecto: $(date)\n"
   git push origin main
-  echo "\nProyecto actualizado en GitHub.\n"
+  echo -e "\nProyecto actualizado en GitHub.\n"
 fi
 
 # Regenerar página web
-echo "\nCreando página web...\n"
+echo -e "\nCreando página web...\n"
 hugo -d ../blogdani/docs
 
 # Subir página
-echo "\nSincronizando página web...\n"
+echo -e "\nSincronizando página web...\n"
 cd ../blogdani/docs
 git add .
 
 # Solo commits con cambios
 if git diff-index --quiet HEAD --; then
-  echo "\nSin cambios que subir.\n"
+  echo -e "\nSin cambios que subir.\n"
 else
   git commit -m "\nActualización del sitio: $(date)\n"
   git push origin main
-  echo "\nPágina web sincronizada en GitHub. Puedes acceder a ella aquí: https://danibacorro.github.io/blogdani/\n"
+  echo -e "\nPágina web sincronizada en GitHub. Puedes acceder a ella aquí: https://danibacorro.github.io/blogdani/\n"
 fi
 
 # Volver al directorio raíz
